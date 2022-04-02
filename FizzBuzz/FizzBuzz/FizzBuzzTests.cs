e8fedad5ab80638b03cd88c3ce4e4e432f9a4013
JamesWang <jame2408@gmail.com>
2022-04-02 22:13:19 +0800

    FizzBuzz

--- FizzBuzz/FizzBuzz/FizzBuzzTests.cs
+++ FizzBuzz/FizzBuzz/FizzBuzzTests.cs
@@ -10,14 +10,62 @@ namespace FizzBuzz
         }
 
         [Test]
-        public void WhenInput0ShouldBe0()
+        public void WhenInput1ShouldBe1()
         {
-            var input = 0;
+            var input = 1;
             var obj = new FizzBuzzGame();
-            
-            var output = "0";
+
+            var output = "1";
             var result = obj.Result(input);
-            
+
+            Assert.AreEqual(output, result);
+        }
+
+        [Test]
+        public void WhenInput3ShouldBeFizz()
+        {
+            var input = 3;
+            var obj = new FizzBuzzGame();
+
+            var output = "Fizz";
+            var result = obj.Result(input);
+
+            Assert.AreEqual(output, result);
+        }
+
+        [Test]
+        public void WhenInput4ShouldBe4()
+        {
+            var input = 4;
+            var obj = new FizzBuzzGame();
+
+            var output = "4";
+            var result = obj.Result(input);
+
+            Assert.AreEqual(output, result);
+        }
+
+        [Test]
+        public void WhenInput5ShouldBeBuzz()
+        {
+            var input = 5;
+            var obj = new FizzBuzzGame();
+
+            var output = "Buzz";
+            var result = obj.Result(input);
+
+            Assert.AreEqual(output, result);
+        }
+
+        [Test]
+        public void WhenInput15ShouldBeFizzBuzz()
+        {
+            var input = 15;
+            var obj = new FizzBuzzGame();
+
+            var output = "FizzBuzz";
+            var result = obj.Result(input);
+
             Assert.AreEqual(output, result);
         }
     }
@@ -26,7 +74,22 @@ namespace FizzBuzz
     {
         public string Result(int input)
         {
-            return "0";
+            if (input % 3 == 0 && input % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+
+            if (input % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            if (input % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            return $"{input}";
         }
     }
 }
